b5aecc5f86102e117982b7163b1af04a20f9b5a4
kkulebaev <konstantinkulebaev@gmail.com>
2022-12-05 13:08:14 +0300

    feat: add test for day1

---
+++ src/day1/day1.spec.js
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest'
+import { findElfCarryingTheMostCalories, findTheTopElvesCarryingTheMostCalories } from './day1.js'
+import { readFile } from './../utils.js'
+
+const inputPath = 'src/day1/input.txt'
+
+const data = readFile(inputPath)
+
+describe('Day 1 (puzzle)', () => {
+  it('part 1 should have answer', () => {
+    const result = findElfCarryingTheMostCalories(data)
+    expect(result).toEqual(72240)
+  })
+
+  it('part 2 should have answer', () => {
+    const result = findTheTopElvesCarryingTheMostCalories(data)
+    expect(result).toEqual(210957)
+  })
+})
